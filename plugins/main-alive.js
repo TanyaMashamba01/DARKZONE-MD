06c25c8f2abc1f2899bc4139ee249e052ced3ff5
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-10-02 19:00:16 +0200

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -35,8 +35,8 @@ async (conn, mek, m, { from, sender, reply }) => {
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363416743041101@newsletter',
-                    newsletterName: '𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟',
+                    newsletterJid: '120363347365643318@newsletter',
+                    newsletterName: '𝐸𝑅𝐹𝒜𝒩MYLOV3',
                     serverMessageId: 143
                 }
             }
